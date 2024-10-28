a69d7bb5ec95f4e91c567f6bfef0f0d822981939
pra-navv <pranavwagholikar09@gmail.com>
2024-10-28 13:05:48 +0530

    created admin interface and added food.

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -7,6 +7,10 @@ class MenuList(generic.ListView):
     queryset = Item.objects.order_by("date_created")
     template_name = "index.html"
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = {"meals": ["Pizza", "Pasta"]}
+        return context
+
 
 class MenuItemDetail(generic.DetailView):
     model = Item
